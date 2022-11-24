e2ed86bd45f30982a8277aed0a35db98ff4c83fd
Ankit Tejbahadur Yadav <ankittejyadav@gmail.com>
2022-11-24 14:54:15 -0500

    Add files via upload

---
+++ tron/Ecorypto/EcoryptoUsers.sol
@@ -0,0 +1,218 @@
+// SPDX-License-Identifier: GPL-3.0
+
+
+pragma solidity >=0.7.0 <0.9.0;
+
+interface ITRC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract EcoryptoUsers {
+    
+//     struct User{
+//         uint  id;
+//         string name;
+//         string ecotype;  // walk,car,
+//         uint price;
+//         // address ad;
+//         bool set; // This boolean is used to differentiate between unset and zero struct values
+//   }
+//   mapping(address => User) public users;
+
+// // define the balance of a user 
+// mapping(uint64 => uint64) idToPoint;
+
+// //define an user balance model
+// mapping (address => uint256) public balances;
+
+
+// function createUser(address ad, uint id, string name) public {
+//     User storage user = users[_userAddress];
+//     // Check that the user did not already exist:
+//     require(!user.set);
+//     //Store the user
+//     users[_userAddress] = User({
+//         id: _userId,
+//         name: _userName,
+//         set: true
+//     });
+// }
+// function CalWalk{
+//     //calculate method
+//     n=n/1000
+// }
+// function CalResoure{
+//     //calculate method
+// }
+// function GetData{
+//     //return result
+// }
+// function count(n) public{
+//     count=n/1000 * trx;
+// }
+
+//address public owner = address(0x15D817C681AE1504DE62cd17C7ACF0f9A62d12CA);
+address public owner;
+event claimReward(address to,uint256 amount);
+    
+constructor() {
+    owner = address(msg.sender);
+}
+
+
+// List of possible eco-friendly actions that users can take
+// very specific tasks ex: Walking to work, Using a reusable water bottle, picking up litter
+// Car-free day (Choose to walk, bike or take public transport to reduce emissions!)
+// 50
+// 4 minute shower challenge (Save water and money at the same time)
+// 50
+// Take your own container (Bring your own bottle to get coffee or your own container to get lunch)
+// 100
+// Eat less emeat (Swap a meal with a tasty veggie alternative)
+// 50
+// Let’s switch the lights off (Remember to turn the lights off when going out)
+// 20
+// Fight food waste (Think carefully about what you buy and try composting!)
+// 50
+// Turn the heating down (Lower your heating by a few degrees to save energy and money)
+// 20
+
+function sendRewardWalk(address user,address token) public {
+    require(tx.origin == owner, "only owner");
+    uint256 amount = 10*10**6; // 10 token
+    ITRC20(token).transfer(user,amount);
+    emit claimReward(user, amount);
+}
+  
+  
+     
+
+    
+function sendRewardCar(address user,address token) public {
+    require(tx.origin == owner, "only owner");
+    uint256 amount = 50*10**6; // 20 token
+    ITRC20(token).transfer(user,amount);
+    //user.claimedReward+=amount;
+    emit claimReward(user, amount);
+}
+
+function sendRewardMeat(address user,address token) public {
+    require(tx.origin == owner, "only owner");
+    uint256 amount = 50*10**6; // 20 token
+    ITRC20(token).transfer(user,amount);
+    //user.claimedReward+=amount;
+    emit claimReward(user, amount);
+}
+
+
+
+function sendRewardFood(address user,address token) public {
+    require(tx.origin == owner, "only owner");
+    uint256 amount = 50*10**6; // 20 token
+    ITRC20(token).transfer(user,amount);
+    //user.claimedReward+=amount;
+    emit claimReward(user, amount);
+}
+
+
+
+function sendRewardContainer(address user,address token) public {
+    require(tx.origin == owner, "only owner");
+    uint256 amount = 100*10**6; // 20 token
+    ITRC20(token).transfer(user,amount);
+    //user.claimedReward+=amount;
+    emit claimReward(user, amount);
+}
+ 
+function sendRewardPublicTrans(address user,address token) public {
+    require(tx.origin == owner, "only owner");
+    uint256 amount = 2*10**6; // 20 token
+    ITRC20(token).transfer(user,amount);
+    //user.claimedReward+=amount;
+    emit claimReward(user, amount);
+}
+
+
+function sendRewardHeat(address user,address token) public {
+    require(tx.origin == owner, "only owner");
+    uint256 amount = 20*10**6; // 20 token
+    ITRC20(token).transfer(user,amount);
+    //user.claimedReward+=amount;
+    emit claimReward(user, amount);
+}
+
+//check the balance
+function getBalance(address token,address user) public view  returns (uint value){
+    return ITRC20(token).balanceOf(user);
+}
+
+
+
+}
---
+++ tron/README.md
@@ -0,0 +1,96 @@
+# Ecorypto
+
+In our ecosystem, we issued our own TRC20 token called ECO and crafted NFT (TRC721) for the avatar costumes and equipment. Users can use tokens to buy NFTs.
+EcoryptoNFT (ECON) (TRC721)
+Our NFT follows the TRC721 ​​standard and stores the images on the Pinata IPFS system. Users can use ECO to mint and buy NFTs, or they can sell NFTs to get ECOs.
+
+Ecorypto (ECO) (TRC20)
+The total supply is 1000000000 with 6 decimals. Our goal is to make a more eco-friendly environment, and it is crucial that we cannot make huge consumption and carbon emissions when we do the transaction. Tron is a more eco-friendly blockchain compared to the other PoS chain.
+
+      (3)Users actions reward 
+
+A.Motivation: can record their actions through transactions,get related reward/incentives,and they also can mint NFT for sale.
+
+B.User data verification:
+  access the data from iwatch
+  access data from third-party app( such as car/gas usage,recycle  trade)
+
+C. Record their actions (  Record the users action and reward them)
+          Step 1: We define some eco-friendliness actions type such as walk/step/Electric car/Resource recycle,and send the users fixed reward by the application organizer.( Through TronIDE to deploy the contract)
+         Step 2: Got the complete action event and it will call the different reward function of the smart contract( Tronweb to connect the UI and smart contract,embedded ABI of contract)
+
+       (4) Frontend
+	We used bootstrap and tronweb to interact with our smartcontracts.
+
+
+I. a eco-friendly credit system 
+
+
+Overview of idea so far: 
+
+Short description (300 characters): 
+Ecorypto aims to incentivize users to build a more sustainable lifestyle. By completing up to 4 eco-friendly challenges per day, users can collect eco-coins to purchase and trade accessory NFTs. Users purchase these NFTs to level up and personalize their avatars to show off to friends and family.
+
+Challenge module: 
+List of possible eco-friendly actions that users can take
+very specific tasks ex: Walking to work, Using a reusable water bottle, picking up litter
+Car-free day (Choose to walk, bike or take public transport to reduce emissions!)
+50
+4 minute shower challenge (Save water and money at the same time)
+50
+Take your own container (Bring your own bottle to get coffee or your own container to get lunch)
+100
+Eat less emeat (Swap a meal with a tasty veggie alternative)
+50
+Let’s switch the lights off (Remember to turn the lights off when going out)
+20
+Fight food waste (Think carefully about what you buy and try composting!)
+50
+Turn the heating down (Lower your heating by a few degrees to save energy and money)
+20
+or categories where users can specify more in detail
+More ecofriendly travel (Took public transport instead of driving, drove with electric car instead of gas car, biked to work)
+Personal life, food, reducing energy
+or just let users type in their own actions
+Challenge completion: 
+give out currency for each challenge completed
+idea: more challenges completed more currency awarded?
+idea: specific challenges give out more rewards?
+get and accumulate carbon credit
+buy carbon credit from certain companies
+distribute among users 
+sell carbon credit to companies 
+get coupons from eco-friendly brands (connect with real world)
+
+currency (! let’s not just generically call this currency), NFTs, marketplace, and level ups: 
+users use currency to buy NFTs to personalize avatar
+NFTs can give your avatar specific weapons or armor or accessories
+marketplace has a generic list of simple accessories and limited NFTs
+level ups can increase your rate of getting currency
+Users can buy currency or NFTs or level ups
+trading NFTs
+sell currency to buy carbon credit to certain companies 
+
+
+
+avatar 
+compete against friends 
+compete in weekly tournaments 
+vote for the best outfit
+level up stats 
+
+collaboration (we can put it in presentation)
+eco-friendly brands
+
+the action related to climate trade 
+
+II. Smart contract function
+
+
+A possible solution is for major blockchain based projects to collaborate with climatetrade
+
+
+Users: 
+Motivation: can record their actions through transactions,get related reward/incentives,and they also can mint NFT for sale.
+
+walk, bus ticket as the certificate 
